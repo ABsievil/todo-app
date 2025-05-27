b9cdc67a7d4ae58bc27022a2df8d154e97cc2710
ABsievil <nccuong317qb@gmail.com>
2025-05-27 16:25:42 +0700

    docs: config main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Set timezone for the application
+  process.env.TZ = 'Asia/Ho_Chi_Minh';
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
