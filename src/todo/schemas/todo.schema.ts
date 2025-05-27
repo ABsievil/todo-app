af3b365b848029c26b0bc2fb60550103844a9067
ABsievil <nccuong317qb@gmail.com>
2025-05-27 16:26:09 +0700

    docs(todo-schema): config hcm timezone

--- src/todo/schemas/todo.schema.ts
+++ src/todo/schemas/todo.schema.ts
@@ -5,7 +5,24 @@ import { TodoPriority } from '../enums/todo-priority.enum';
 
 export type TodoDocument = Todo & Document;
 
-@Schema({ timestamps: true })
+@Schema({ 
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      if (ret.createdAt) {
+        const date = new Date(ret.createdAt);
+        date.setHours(date.getHours() + 7);
+        ret.createdAt = date.toISOString();
+      }
+      if (ret.updatedAt) {
+        const date = new Date(ret.updatedAt);
+        date.setHours(date.getHours() + 7);
+        ret.updatedAt = date.toISOString();
+      }
+      return ret;
+    }
+  }
+})
 export class Todo {
   @Prop({ required: true })
   title: string;
